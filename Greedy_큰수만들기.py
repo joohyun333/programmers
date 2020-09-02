3f58bf32f4b804cb96f8f4957573c30c83b171c1
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2020-09-03 06:40:41 +0900

    Update Greedy_큰수만들기.py

--- Greedy_큰수만들기.py
+++ Greedy_큰수만들기.py
@@ -1,10 +1,23 @@
-def solution(number, k):
+# https://programmers.co.kr/learn/courses/30/lessons/42883
+def solution(number, K):
     number = list(number)
+    k = len(number) - K
     answer = []
-
-    return number,answer
+    answer.append(max(number[:-(k - 1)]))
+    k -= 1
+    while len(answer) != len(number)-K:
+        number = number[number.index(str(answer[-1:][0]))+1:]
+        if k == 1:
+            answer = answer + number[-1:]
+            break
+        answer.append(max(number[0:-(k-1)]))
+        k -= 1
+        if k == 1 :
+            answer = answer+number[-1:]
+            break
+    return ''.join(answer)
 
 if __name__ == "__main__":
-    number = "1231234"
-    k = 3
-    print(solution(number, k))
+    number = "1924"
+    k = 2
+    print(solution(number, k)) #775841
