e8ec1beecffc65d99100c4e9d46e3785f496e0a8
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2020-09-10 06:06:20 +0900

    Create 12930.py

---
+++ E_2020_09_10/12930.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12930
+def solution(s):
+    answer = list(s.split(" "))
+    a = ""
+    for i in range(len(answer)):
+        for j in range(len(answer[i])):
+            if j % 2 == 0:
+                a+=answer[i][j].upper()
+            else:
+                a+=answer[i][j].lower()
+        a += " "
+    return a[:-1]
+
+if __name__ == "__main__":
+    s ="try hello world"
+    print(solution(s))
