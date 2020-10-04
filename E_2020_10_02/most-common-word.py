68347015652c583b875e85d7c29c9c6210651ac6
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2020-10-04 22:05:50 +0900

    study

--- E_2020_10_02/most-common-word.py
+++ E_2020_10_02/most-common-word.py
@@ -1,15 +1,11 @@
 from typing import List
 import collections
 import string
+import re
 def mostCommonWord(paragraph: str, banned: List[str]) -> str:
-    strs = ''.join([i.lower() for i in paragraph if i not in string.punctuation])
-    result = collections.Counter(strs.split()).most_common()
-    for results in result:
-        if results[0] == banned[0]:
-            pass
-        else:
-            return results[0]
-    return result
+    words = [word for word in re.sub(r"[^\w]", ' ', paragraph).lower().split() if word not in banned]
+    counts = collections.Counter(words)
+    return counts.most_common(1)[0][0]
 if __name__ == "__main__":
     paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
     banned = ["hit"]
---
+++ E_2020_10_04/about_re.py
@@ -0,0 +1,16 @@
+# https://greeksharifa.github.io/%EC%A0%95%EA%B7%9C%ED%91%9C%ED%98%84%EC%8B%9D(re)/2018/07/21/regex-usage-02-basic/
+import re
+matchObj = re.findall('\w\W\w', 'a (9_a a')
+print(matchObj) # ['a a']
+matchObj = re.search('\w\W\w', 'a (9_a a')
+print(matchObj) # <re.Match object; span=(5, 8), match='a a'>
+matchObj = re.findall(r'\w\b\W\B', 'ab  c d  == = e= =f')
+print(matchObj) # ['b ', 'd ', 'e=']
+print(re.findall('\A ryan\d\s\Z', ' ryan1 \n ryan2 \n rain1 \n ryan3 ')) #[]
+print(re.findall('\A ryan\d\s\Z', ' ryan1 ')) # [' ryan1 ']
+print("\\")
+print(r"\"",r"\\")
+matchObj = re.findall(r'\bone\b|\boneself\b|\bonerous\b', 'oneself is the one thing.')
+print(matchObj)
+matchObj = re.findall('oneself|one|onerous', 'oneself is the one thing.')
+print(matchObj)
