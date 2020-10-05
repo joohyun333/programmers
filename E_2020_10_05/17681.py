c86f7565ed400a4519e68d13a2cd062fc3174926
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2020-10-05 18:27:55 +0900

    Update 17681.py

--- E_2020_10_05/17681.py
+++ E_2020_10_05/17681.py
@@ -8,21 +8,25 @@ def solution(n, arr1, arr2):
             arr_1.append(0)
         else:
             arr_1.append(int(bin(i).lstrip('0b')))
+    # arr_1 = [1001, 10100, 11100, 10010, 1011]
     for i in arr2:
         if i == 0:
             arr_2.append(0)
         else:
             arr_2.append(int(bin(i).lstrip('0b')))
+    # arr_2 = [11110, 1, 10101, 10001, 11100]
     for i in range(n):
         result[i] = str(arr_1[i] + arr_2[i]).rjust(n,'0')
         answer.append(''.join(re.sub('[0]'," ",word) for word in re.sub('[1-2]',"#", result[i])))
+    # result = ['12111', '10101', '21201', '20011', '12111']
+    # answer = ['#####', '# # #', '### #', '#  ##', '#####']
     return answer
 
 if __name__ == "__main__":
-    # n = 5
-    # arr1 , arr2 = [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]
+    n = 5
+    arr1 , arr2 = [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]
     # n = 6
     # arr1, arr2 = [27, 56, 19, 14, 14, 10], [46, 33, 33, 22, 31, 50]
-    n = 6
-    arr1, arr2 = [0, 0, 0, 10, 0, 0], [46, 33, 33, 22, 31, 50]
+    # n = 6
+    # arr1, arr2 = [0, 0, 0, 10, 0, 0], [46, 33, 33, 22, 31, 50]
     print(solution(n, arr1, arr2))
