91278dbd7a1f502228a1b30dc5107529677c232f
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2020-11-19 20:07:26 +0900

    s

---
+++ 문자열 압축.py
@@ -0,0 +1,26 @@
+# https://programmers.co.kr/learn/courses/30/lessons/60057
+def solution(s:str) -> int:
+    if len(s) < 2:
+        return len(s)
+    string = [s[0:i] for i in range(1,(len(s)//2)+1)]
+    answer = []
+    for i in range(len(string)):
+        num = 0
+        result=[]
+        for j in range(0,len(s),len(string[i])):
+            if string[i] == s[j:j+len(string[i])]:
+                num += 1
+            else:
+                result.append(num)
+                string[i] = s[j:j+len(string[i])]
+                num = 1
+        result.append(num)
+        index = [i for i in result if i>1]
+        resultstr = s + ''.join(map(str,index))
+        j = (i+1) * sum([flex-1 for flex in index])
+        answer.append(len(resultstr)-j)
+    return min(answer)
+
+if __name__ == "__main__":
+    s = "aabbaccc"
+    print(solution(s))
--- 풀었던 문제들/kakao2.py
+++
@@ -1,1 +0,0 @@
-# https://programmers.co.kr/tryouts/16422/challenges
