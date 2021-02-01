edf09449ac119af74ca67dcd430bdeeb28c5138d
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2021-02-01 21:03:56 +0900

    DP

---
+++ 백준/계단오르기.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/2579
+N = int(input())
+stairs = []
+for i in range(N):
+    stairs.append(int(input()))
+stairs_dp = []
+if len(stairs) < 3 :
+    print(sum(stairs))
+else:
+    stairs_dp.append(stairs[0])
+    stairs_dp.append(max(stairs[0]+stairs[1], stairs[1]))
+    stairs_dp.append(max(stairs[0]+stairs[2], stairs[1]+stairs[2]))
+    for i in range(3, N):
+        temp  = max(stairs[i]+stairs[i-1]+stairs_dp[i-3], stairs[i]+stairs_dp[i-2])
+        stairs_dp.append(temp)
+    print(stairs_dp[N-1])
+
