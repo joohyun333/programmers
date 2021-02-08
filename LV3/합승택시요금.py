9bfee4bf88d3e90ac39b8c9e8f46f5aaf0fa6cb2
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2021-02-09 00:47:21 +0900

    2021 KAKAO BLIND RECRUITMENT - 극한의 Dijkstra

--- LV3/합승택시요금.py
+++ LV3/합승택시요금.py
@@ -1,16 +1,19 @@
 # https://programmers.co.kr/learn/courses/30/lessons/72413
 import collections
 import heapq
-def solution(n, s, a, b, fares):
-    graph = collections.defaultdict(list)
+from typing import List, Deque, DefaultDict
+
+
+def solution(n: int, s: int, a: int, b: int, fares: List[int]) -> int:
+    graph: DefaultDict[int, int, int] = collections.defaultdict(list)
     for start, dis, fee in fares:
         graph[start].append((dis, fee))
         graph[dis].append((start, fee))
 
-    def Dijkstra(s, d):
-        queue = [(0, s)]
-        discoverd = [False] * (n + 1)
-        dis = collections.defaultdict(list)
+    def Dijkstra(s: int, d: int) -> DefaultDict[int, int]:
+        queue: List[int, int] = [(0, s)]
+        discoverd: List[bool] = [False] * (n + 1)
+        dis: DefaultDict[int, int] = collections.defaultdict(list)
         while queue:
             node_fee, node = heapq.heappop(queue)
             if node == d:
@@ -24,22 +27,22 @@ def solution(n, s, a, b, fares):
                     heapq.heappush(queue, (new_fee, i))
         return dis
 
-    mid_Dijkstra = Dijkstra(s, n + 1)
-    mid = collections.deque(mid_Dijkstra.items())
-    min_fees = mid_Dijkstra[a] + mid_Dijkstra[b]
+    mid_Dijkstra: DefaultDict[int, int] = Dijkstra(s, n + 1)
+    mid: Deque = collections.deque(mid_Dijkstra.items())
+    min_fees: int = mid_Dijkstra[a] + mid_Dijkstra[b]
     mid.popleft()
     for i, e in mid:
         a_ = Dijkstra(i, a)[a]
         b_ = Dijkstra(i, b)[b]
-        m = e+a_+b_
-        if m < min_fees: min_fees=m
+        m = e + a_ + b_
+        if m < min_fees: min_fees = m
     return min_fees
 
 
 if __name__ == "__main__":
     n, s, a, b, fares = 6, 4, 6, 2, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66],
-                                     [2, 3, 22], [1, 6, 25]]# 82
-    n, s, a, b, fares = 7, 3, 4, 1, [[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]] # 14
+                                     [2, 3, 22], [1, 6, 25]]  # 82
+    n, s, a, b, fares = 7, 3, 4, 1, [[5, 7, 9], [4, 6, 4], [3, 6, 1], [3, 2, 3], [2, 1, 6]]  # 14
     # n, s, a, b, fares = 6, 4, 5, 6, [[2, 6, 6], [6, 3, 7], [4, 6, 7], [6, 5, 11], [2, 5, 12], [5, 3, 20], [2, 4, 8],
     #                                  [4, 3, 9]]# 18
     print(solution(n, s, a, b, fares))
