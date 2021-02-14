b472f52e6856211018130b5027f0f5d8b2a4acc8
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2021-02-14 18:28:58 +0900

    DP

--- 백준/RGB거리.py
+++ 백준/RGB거리.py
@@ -1,9 +1,13 @@
 # https://www.acmicpc.net/problem/1149
 import sys
+
+input = sys.stdin.readline
 N = int(input())
 RGB = []
 for _ in range(N):
     RGB.append(list(map(int, input().split())))
-print(RGB)
-# for i,e in enumerate(dix):
-#     start = i
+for i in range(1,N):
+    RGB[i][0] = min(RGB[i][0]+RGB[i - 1][1], RGB[i][0]+RGB[i - 1][2])
+    RGB[i][1] = min(RGB[i][1]+RGB[i - 1][0], RGB[i][1]+RGB[i - 1][2])
+    RGB[i][2] = min(RGB[i][2]+RGB[i - 1][0], RGB[i][2]+RGB[i - 1][1])
+print(min(RGB[N-1]))
