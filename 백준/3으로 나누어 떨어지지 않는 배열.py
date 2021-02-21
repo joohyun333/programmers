9549fd3d7c80f22ac79238e984e094ac84f6f50a
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2021-02-21 18:52:58 +0900

    Update 3으로 나누어 떨어지지 않는 배열.py

--- 백준/3으로 나누어 떨어지지 않는 배열.py
+++ 백준/3으로 나누어 떨어지지 않는 배열.py
@@ -1,5 +1,6 @@
-#https://www.acmicpc.net/problem/2938
+# https://www.acmicpc.net/problem/2938
 import collections
+
 N = int(input())
 data = collections.deque(list(map(int, input().split())))
 stack = []
@@ -7,23 +8,25 @@ while data:
     # data_N = len(data)
     v = data.popleft()
     if not stack:
-        if data[0]+v % 3 != 0:
-           stack.append(v)
-           stack.append(data.popleft())
+        if data[0] + v % 3 != 0:
+            stack.append(v)
+            stack.append(data.popleft())
         else:
             data.append(v)
-    elif stack[-1]+v % 3 != 0:
+    elif stack[-1] + v % 3 != 0:
         stack.append(v)
-    else:data.append(v)
+    else:
+        data.append(v)
     print(data, stack)
 
 input()
-a=[[],[],[]]
-for v in map(int,input().split()):a[v%3]+=v,
-if len(a[0])>1+len(a[1]+a[2])or (a[1] and a[2] and not a[0]):print(-1)
+a = [[], [], []]
+for v in map(int, input().split()): a[v % 3] += v,
+if len(a[0]) > 1 + len(a[1] + a[2]) or (a[1] and a[2] and not a[0]):
+    print(-1)
 else:
- r=[]
- for v in a[0]:
-  if r:r+=(a[1]or a[2]).pop(),
-  r+=v,
- print(*a[1]+r+a[2])
+    r = []
+    for v in a[0]:
+        if r: r += (a[1] or a[2]).pop(),
+        r += v,
+    print(*a[1] + r + a[2])
