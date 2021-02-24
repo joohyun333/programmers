7b0fbe5fcb96e7feff14e8a3f0e98c108a0b9749
joohyun333 <58985553+joohyun333@users.noreply.github.com>
2021-02-25 04:24:11 +0900

    PS

---
+++ 백준/크루스칼/네크워크 연결.py
@@ -0,0 +1,33 @@
+import sys
+import heapq
+input = sys.stdin.readline
+N = int(input())
+M = int(input())
+route = []
+discoverd = [False]*(N+1)
+for _ in range(M):
+    a,b,c = map(int, input().split())
+    route.append((c,a,b))
+parent = [i for i in range(N+1)]
+
+def search_parent(parent, n):
+    if parent[n] != n:
+        parent[n] = search_parent(parent, parent[n])
+    return parent[n]
+
+def union_parent(parent,a,b):
+    x = search_parent(parent, a)
+    y = search_parent(parent, b)
+    if x<y:
+        parent[y] = x
+    else:parent[x] = y
+
+heapq.heapify(route)
+result = 0
+while route:
+    time, start, end = heapq.heappop(route)
+    if search_parent(parent, start) != search_parent(parent, end):
+        union_parent(parent, start, end)
+        result+=time
+
+print(result)
