6da710255e4549684e6420c59d53dbf32115b8b3
JOOHYUNZZANG\dlwngus <joohyun333@naver.com>
2022-01-12 05:45:59 +0900

    DP

--- javacodingtest/src/다이나믹프로그래밍/BOJ14501퇴사.java
+++ javacodingtest/src/다이나믹프로그래밍/BOJ14501퇴사.java
@@ -2,51 +2,32 @@ package 다이나믹프로그래밍;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BOJ14501퇴사 {
-
-
-    static int comparison(int a, int b, int c){
-        return (a > b) ? ((a > c) ? a : c) : (b > c) ? b : c;
-    }
-
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder();
+        int totalDay = Integer.parseInt(bufferedReader.readLine());
 
-        String[] size = bufferedReader.readLine().split(" ");
-        int row = Integer.parseInt(size[0]);
-        int column = Integer.parseInt(size[1]);
+        int[] period = new int[totalDay+1];
+        int[] money = new int[totalDay+1];
+        // 결과값
+        int[] result = new int[totalDay+1];
 
-        int[][] maze = new int[row][column];
-        int[][] result = new int[row][column];
+        for (int i = 0; i < totalDay; i++) {
+            String[] data = bufferedReader.readLine().split(" ");
+            period[i] = Integer.parseInt(data[0]);
+            money[i] = Integer.parseInt(data[1]);
+        }
 
-        for (int i = 0; i < row; i++) {
-            String[] candy = bufferedReader.readLine().split(" ");
-            for (int j = 0; j < column; j++) {
-                maze[i][j] = Integer.parseInt(candy[j]);
+
+        for (int i = totalDay-1 ; i >= 0; i--) {
+            if (period[i] + i <= totalDay) {
+                result[i] = Integer.max(money[i] + result[i + period[i]], result[i + 1]);
+            }else {
+                result[i] = result[i+1];
             }
         }
-
-        result[0][0] = maze[0][0];
-
-        for (int i = 1; i < column; i++) {
-            result[0][i] = maze[0][i] + result[0][i-1];
-        }
-
-        for (int i = 1; i < row; i++) {
-            result[i][0] = maze[i][0] + result[i-1][0];
-        }
-
-        if (row > 1 && column > 1) {
-            for (int i = 1; i < row; i++) {
-                for (int j = 1; j < column; j++) {
-                    result[i][j] = maze[i][j] + comparison(result[i-1][j-1], result[i-1][j], result[i][j-1]);
-                }
-            }
-        }
-
-        System.out.println(result[row-1][column-1]);
+        System.out.println(result[0]);
     }
 }
