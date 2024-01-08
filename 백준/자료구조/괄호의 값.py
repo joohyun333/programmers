ddcc9ba6dfa0640cbc3e266e77540a573bca9285
joohyun333 <joohyun333@naver.com>
2024-01-09 03:19:34 +0900

    스택 공부... 다시 풀어볼 문제

--- 백준/자료구조/괄호의 값.py
+++ 백준/자료구조/괄호의 값.py
@@ -1,15 +1,35 @@
-#https://www.acmicpc.net/problem/2504
+# https://www.acmicpc.net/problem/2504
 from collections import deque
 
-data = list(input())
-stack = deque()
+data = input()
+temp = 1
 answer = 0
-while data:
-    data.
+stack = []
+for idx, d in enumerate(data):
+    if d == "(" :
+        temp *= 2
+        stack.append(d)
+    if d == "[":
+        temp *= 3
+        stack.append(d)
+    elif d == ")":
+        if not stack or stack[-1] != "(":
+            answer = 0
+            break
+        if data[idx - 1] == "(":
+            answer += temp
+        stack.pop()
+        temp = temp // 2
 
-for d in data:
-    if d == "(":
-        stack.append()
-
-    elif d == "[":
-    deque[]
+    elif d == "]":
+        if not stack or stack[-1] != "[":
+            answer = 0
+            break
+        if data[idx - 1] == "[":
+            answer += temp
+        stack.pop()
+        temp = temp // 3
+if stack:
+    print(0)
+else:
+    print(answer)
